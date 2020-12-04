abef8ef8f7fdb7a613c525aa1b6d253305963006
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2020-12-04 23:58:28 +0100

    opdracht 6

--- js/javaScript.js
+++ js/javaScript.js
@@ -6,3 +6,16 @@ document.querySelector('.animeer-knop').addEventListener('click', (e) =>{
 setTimeout( () => { 
  document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts');
 }, 300);
+
+//Nav button klik: 
+const menuKnop        =  document.querySelector('.nav__knop');
+const navigationItems =  document.querySelectorAll('.nav__link');
+
+ menuKnop.addEventListener( 'click', () => {
+  navigationItems.forEach( (item, index) => {
+  
+   setTimeout( () => { 
+    item.classList.toggle('nav__link--schuif-in');
+    }, 100 * index);
+  })
+ });
