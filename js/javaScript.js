34051d8def567316180be5845dab45b31f0ef68f
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2020-12-05 00:18:13 +0100

    opdracht 7

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,5 +1,5 @@
-document.querySelector('.animeer-knop').addEventListener('click', (e) =>{
- document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts');
+document.querySelector( '.animeer-knop' ).addEventListener( 'click', (e) =>{
+ document.querySelector( '.vlakje' ).classList.toggle( 'vlakje__naar-rechts' );
 });
 
 //Na het laden van de pagina, start de animatie van de Main blokje
@@ -8,14 +8,17 @@ setTimeout( () => {
 }, 300);
 
 //Nav button klik: 
-const menuKnop        =  document.querySelector('.nav__knop');
-const navigationItems =  document.querySelectorAll('.nav__link');
+const menuKnop        =  document.querySelector( '.nav__knop' );
+const navigationItems =  document.querySelectorAll( '.nav__link' );
 
  menuKnop.addEventListener( 'click', () => {
   navigationItems.forEach( (item, index) => {
   
    setTimeout( () => { 
-    item.classList.toggle('nav__link--schuif-in');
+    item.classList.toggle( 'nav__link--schuif-in' );
     }, 100 * index);
-  })
+  });
+  document.querySelector( '.hamburger_button' ).classList.toggle( 'verbergen' );
+  document.querySelector( '.sluitKnop' ).classList.toggle( 'verbergen' );
+  
  });
