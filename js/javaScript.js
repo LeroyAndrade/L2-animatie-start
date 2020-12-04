399de98be7d1c6cadd4a7ffaf96b6cf28b5267f9
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2020-12-04 23:46:35 +0100

    opdracht 5

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,3 +1,8 @@
 document.querySelector('.animeer-knop').addEventListener('click', (e) =>{
  document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts');
 });
+
+//Na het laden van de pagina, start de animatie van de Main blokje
+setTimeout( () => { 
+ document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts');
+}, 300);
